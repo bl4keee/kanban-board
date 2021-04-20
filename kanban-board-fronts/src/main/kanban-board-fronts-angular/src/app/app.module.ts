19199181e0b8107fe99c2b9bc96c87ed5ff2d35f
Bartek <bl4kee@wp.pl>
2021-04-20 09:56:12 +0200

    Added routing and main-view component.

--- kanban-board-fronts/src/main/kanban-board-fronts-angular/src/app/app.module.ts
+++ kanban-board-fronts/src/main/kanban-board-fronts-angular/src/app/app.module.ts
@@ -2,13 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MainViewComponent } from './components/main-view/main-view.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainViewComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
