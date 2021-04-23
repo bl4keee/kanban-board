5df656c7cc7212e8fc8c6778a524c7cef5164dbc
Bartek <bl4kee@wp.pl>
2021-04-23 23:39:20 +0200

    Updated main-view component.

--- kanban-board-fronts/src/main/kanban-board-fronts-angular/src/app/components/main-view/main-view.component.ts
+++ kanban-board-fronts/src/main/kanban-board-fronts-angular/src/app/components/main-view/main-view.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {CdkDragDrop, moveItemInArray, transferArrayItem} from '@angular/cdk/drag-drop';
+import {Board} from "../../models/board.model";
+import {Column} from "../../models/column.model";
 
 @Component({
   selector: 'app-main-view',
@@ -9,7 +12,33 @@ export class MainViewComponent implements OnInit {
 
   constructor() { }
 
+  board: Board = new Board('Test Board', [
+    new Column('Todo', [
+      'Some random task',
+      'This is another task'
+    ]),
+    new Column('In progress', [
+      'Task A',
+      'Task B',
+      'Task C'
+    ]),
+    new Column('Done', [
+      'Task D',
+      'Task E'
+    ])
+  ]);
+
   ngOnInit(): void {
   }
 
+  drop(event: CdkDragDrop<string[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex);
+    }
+  }
 }
